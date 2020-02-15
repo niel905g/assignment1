1f21b7fe31dd2006b887f4b921f295bc717b074e
niel905g <niel905g@edu.easj.dk>
2020-02-15 13:51:56 +0100

    Ratings update

--- src/app/rating.component.ts
+++ src/app/rating.component.ts
@@ -1,43 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'rating',
-  template: `
-  <i
-  class="glyphicon"
-  [class.glyphicon-star-empty]="rating < 1"
-  [class.glyphicon-star]="rating >= 1"
-  (click)="onClick(1)">
-  </i>
-  <i
-  class="glyphicon"
-  [class.glyphicon-star-empty]="rating < 2"
-  [class.glyphicon-star]="rating >= 2"
-  (click)="onClick(2)">
-  </i>
-  <i
-  class="glyphicon"
-  [class.glyphicon-star-empty]="rating < 3"
-  [class.glyphicon-star]="rating >= 3"
-  (click)="onClick(3)">
-  </i>
-  <i
-  class="glyphicon"
-  [class.glyphicon-star-empty]="rating < 4"
-  [class.glyphicon-star]="rating >= 4"
-  (click)="onClick(4)">
-  </i>
-  <i
-  class="glyphicon"
-  [class.glyphicon-star-empty]="rating < 5"
-  [class.glyphicon-star]="rating >= 5"
-  (click)="onClick(5)">
-  </i>
-  `
+  templateUrl: 'rating.component.html',
+  styles: [`
+        .glyphicon-star {
+            color: orange;
+        }
+    `]
 })
 export class RatingComponent {
-  rating = 0;
+  // tslint:disable-next-line: no-input-rename
+  @Input('rating-value') rating = 0;
+  @Input() numOfReviews = 0;
 
   onClick(ratingValue) {
     this.rating = ratingValue;
